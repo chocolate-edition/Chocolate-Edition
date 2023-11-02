73981c1f02327041ea2deb600c5cda1a95a15664
Toxin4426 <Toxin4426@gmail.com>
2023-11-01 21:00:51 -0400

    Replace Beetroot in Loot Tables
    with Farmers Delights Seeds

--- overrides/kubejs/server_scripts/script.js
+++ overrides/kubejs/server_scripts/script.js
@@ -13,3 +13,21 @@ ServerEvents.tags('item', event => {
 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 })
+
+LootJS.modifiers((event) => {
+    event
+        .addLootTypeModifier(LootType.CHEST) // or multiple LootType.BLOCK, LootType.ENTITY ... 
+        .removeLoot('minecraft:beetroot_seeds')
+      	.addAlternativesLoot(
+            LootEntry.of('farmersdelight:onion', 5).when((c) => c.randomChance(0.01)),
+	    LootEntry.of('farmersdelight:tomato_seeds', 5).when((c) => c.randomChance(0.01)),
+	    LootEntry.of('farmersdelight:cabbage_seeds', 5).when((c) => c.randomChance(0.01)),
+	    LootEntry.of('farmersdelight:rice', 5).when((c) => c.randomChance(0.01)),
+
+  	    LootEntry.of('farmersdelight:onion', 3).when((c) => c.randomChance(0.02)),
+	    LootEntry.of('farmersdelight:tomato_seeds', 3).when((c) => c.randomChance(0.02)),
+	    LootEntry.of('farmersdelight:cabbage_seeds', 3).when((c) => c.randomChance(0.02)),
+	    LootEntry.of('farmersdelight:rice', 3).when((c) => c.randomChance(0.02)))
+
+    })
+
