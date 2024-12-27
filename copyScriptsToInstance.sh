5002feb0ce2162ef67402d9b1e6f9b9d6929d82a
Jonah Hansen <tce_mc@telus.net>
2024-12-27 13:54:55 -0600

    update gitignore and bash scripts to support /src/ folder

--- copyScriptsToInstance.sh
+++ copyScriptsToInstance.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# copies the contents of ./overrides/scripts to your development instance of minecraft
+# copies the contents of ./src/overrides/scripts to your development instance of minecraft as specified in instancePath.txt
 
 path=$(head -n 1 instancePath.txt)
 
 echo "deleting scripts"
 rm -rf "$path/scripts"
 echo copying scripts to $path
-cp -r ./overrides/scripts "$path"
+cp -r ./src/overrides/scripts "$path"
--- copyToInstance.sh
+++ copyToInstance.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-# copies the contents of ./overrides to your development instance of minecraft
-# ? make a copy of this file and rename to copyToInstance.sh then replace <your instance path> with the absolute path to your minecraft instance.
-# example path: 
-# C:\Users\user\curseforge\minecraft\Instances\dev build The Chocolate Edition - [FORGE]
+# copies the contents of ./src/overrides to your development instance of minecraft as specified in instancePath.txt
 
 path=$(head -n 1 instancePath.txt)
 
 # Loop through each folder in the "overrides" directory
-for folder in "./overrides"/*; do
-    # Extract folder name without path. do not delete config as it contains paxi datapacks
+for folder in "./src/overrides"/*; do
+    # Extract folder name without path.
     if [ -d "$folder" ]; then
         folder_name=$(basename "$folder")
         # Check if the corresponding folder exists in the target directory and delete it
         if [[ -d "$path/$folder_name" ]]; then
-            if [[ "$folder_name" == "defaultconfigs" || "$folder_name" == "scripts" ]]; then
+            if [[ "$folder_name" == "config" || "$folder_name" == "defaultconfigs" || "$folder_name" == "scripts" ]]; then
                 echo "deleting $folder"
                 rm -rf "$path/$folder_name"
             fi
